32bf1159cbf8ed462e6e863b30700c4276efc857
hakobyaann <109814279+hakobyaann@users.noreply.github.com>
2022-09-27 20:15:17 +0400

    Add files via upload

---
+++ Sep-27-2022/ltrim.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> ltrim(std::string);
+
+int main()
+{
+    std::string str = "    hello";
+    std::vector<std::string>result = ltrim(str);
+    for (auto i : result)
+    {
+        std::cout << i;
+    }   
+    
+ return 0;    
+}
+
+std::vector<std::string> ltrim(std::string vec)
+{
+    std::string st = "";
+    int i = 0;
+    std::vector<std::string> result;
+    while (vec[i] != '\0')
+    {
+        if (vec[i] == ' ')
+        {
+            ++i;
+        }
+        else
+        {
+            st += vec[i];
+            ++i;
+        }
+    }
+    result.push_back(st);
+   
+ return result;
+}
